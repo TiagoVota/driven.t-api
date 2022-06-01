3e6547dcee74bd51b9769e7222abf0fa3252a262
Vinicius <vinicunha2008@gmail.com>
2022-06-01 16:34:37 -0300

    fix: seed eslint error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
-import { HotelOption, ModalityName, PrismaClient } from '@prisma/client';
+import { HotelOption, PrismaClient } from '@prisma/client';
 import dayjs from 'dayjs';
+
 const prisma = new PrismaClient();
 
 async function main() {
@@ -17,37 +18,39 @@ async function main() {
     });
   }
 
+  // eslint-disable-next-line no-console
   console.log({ event });
 
-  // CREATE HOTEL OPTIONS
-  // const hotelOptionsList = [
-  //   {
-  //     isWanted: true,
-  //     price: 35000,
-  //   },
-  //   {
-  //     isWanted: false,
-  //     price: 0,
-  //   },
-  // ] as HotelOption[]
+  //CREATE HOTEL OPTIONS
+  const hotelOptionsList = [
+    {
+      isWanted: true,
+      price: 35000,
+    },
+    {
+      isWanted: false,
+      price: 0,
+    },
+  ] as HotelOption[];
 
-	// const hotelOptionsPromises = hotelOptionsList.map(hotelOption => {
-	// 	const promise = prisma.hotelOption.upsert({
-	// 		where: {
-	// 			isWanted: hotelOption.isWanted,
-	// 		},
-	// 		update: {},
-	// 		create: hotelOption,
-	// 	})
+  const hotelOptionsPromises = hotelOptionsList.map((hotelOption) => {
+    const promise = prisma.hotelOption.upsert({
+      where: {
+        isWanted: hotelOption.isWanted,
+      },
+      update: {},
+      create: hotelOption,
+    });
 
-	// 	return promise
-	// })
+    return promise;
+  });
 
-  // await Promise.all(hotelOptionsPromises)
+  await Promise.all(hotelOptionsPromises);
 }
 
 main()
   .catch((e) => {
+    // eslint-disable-next-line no-console
     console.error(e);
     process.exit(1);
   })
