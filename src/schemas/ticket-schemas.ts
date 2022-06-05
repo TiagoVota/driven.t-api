4c2158e6a1d7407a0719c5c977d80153cc3d5e34
pedro <pedrohperoni@gmail.com>
2022-06-05 20:31:18 -0300

    fix: remove userId from request

--- src/schemas/ticket-schemas.ts
+++ src/schemas/ticket-schemas.ts
@@ -2,6 +2,5 @@ import Joi from 'joi';
 import { CreateTicketParams } from '@/services/tickets-service';
 
 export const createTicketSchema = Joi.object<CreateTicketParams>({
-  userId: Joi.number().required(),
   modalityId: Joi.number().required(),
 });
