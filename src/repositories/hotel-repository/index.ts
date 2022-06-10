16afcc7b82b35ded99ba22d44ea6ecdc0da0678d
pedro <pedrohperoni@gmail.com>
2022-06-10 15:17:48 -0300

    feat: create redis cache for hotels

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/config';
+import { prisma, redis } from '@/config';
 
 async function find(hotelId: number) {
   return prisma.hotel.findUnique({
@@ -9,15 +9,21 @@ async function find(hotelId: number) {
 }
 
 async function getHotels() {
-  return prisma.hotel.findMany({
-    include: {
-      Room: {
-        include: {
-          RoomsUsers: true,
+  const cachedHotels = await redis.get('hotels');
+  if (!cachedHotels) {
+    const hotels = await prisma.hotel.findMany({
+      include: {
+        Room: {
+          include: {
+            RoomsUsers: true,
+          },
         },
       },
-    },
-  });
+    });
+    await redis.set('hotels', JSON.stringify(hotels), { EX: 600000 });
+    return hotels;
+  }
+  return JSON.parse(cachedHotels);
 }
 
 const hotelRepository = {
